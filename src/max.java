a7ec0fba1e37133417f8171bee795e4752a0f50f
StepanS <minme4u@ukr.net>
2019-01-21 15:02:15 -0600

    add sout Please enter the commit message for your changes. Lines starting

--- src/max.java
+++ src/max.java
@@ -2,5 +2,6 @@ public class max {
     public static void main(String[] args) {
         System.out.println("Hello Max");
         System.out.println("bla");
+        System.out.println("jj");
     }
 }
