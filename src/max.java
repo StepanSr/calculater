7be0df3f488c8cca7bcc483dcee40247a848ba5b
StepanSr <46882750+StepanSr@users.noreply.github.com>
2019-01-21 15:17:41 -0600

    add true

--- src/max.java
+++ src/max.java
@@ -2,6 +2,8 @@ public class max {
     public static void main(String[] args) {
         System.out.println("Hello Max");
         System.out.println("bla");
-        System.out.println("jj");
+        System.out.println("Max samiy luchiy");
+        
+ 
     }
 }
